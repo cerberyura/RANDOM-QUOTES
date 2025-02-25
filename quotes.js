2c741c9721790f32e47cac1a6c241fcdb62c894f
Yurii Ambroziak <yuriicerber@gmail.com>
2025-02-25 05:48:17 +0000

    Splitting app into the JS module

---
+++ quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+   {
+      quote: 'The only way to do great work is to love what you do',
+      author: 'Steve Jobs',
+   },
+   {
+      quote: 'Your time is limited, so don\'t waste it living someone else\'s life',
+      author: 'Steve Jobs',
+   },
+   {
+      quote: 'Success is not final, failure is not fatal: it is the courage to continue that counts',
+      author: 'Winston Churchill',
+   },
+   {
+      quote: 'Life is what happens when you\'re busy making other plans',
+      author: 'John Lennon',
+   },
+   {
+      quote: 'It does not matter how slowly you go as long as you do not stop',
+      author: 'Confucius',
+   },
+   {
+      quote: 'Do what you can, with what you have, where you are',
+      author: 'Theodore Roosevelt',
+   },
+   {
+      quote: 'Believe you can and you\'re halfway there',
+      author: 'Theodore Roosevelt',
+   },
+   {
+      quote: 'Don\'t watch the clock; do what it does. Keep going',
+      author: 'Sam Levenson',
+   },
+   {
+      quote: 'Opportunities don\'t happen. You create them',
+      author: 'Chris Grosser',
+   },
+   {
+      quote: 'Act as if what you do makes a difference. It does',
+      author: 'William James',
+   }
+];
+
+export default quotes
